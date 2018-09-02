1086c3f4640d9ecefd457b74f8ef659c553e777f
Pablo S. Blum de Aguiar <scorphus@gmail.com>
2018-09-02 18:37:42 +0200

    Implement func to find best link station for device

--- lisle/__init__.py
+++ lisle/__init__.py
@@ -10,4 +10,16 @@
 
 
 def find_best_link_station(link_stations, device_point):
-    return True
+    best_station, max_power = None, 0.0
+    point_x, point_y = device_point
+    for station in link_stations:
+        station_x, station_y, reach = station
+        distance = pow(pow(station_x - point_x, 2) + pow(station_y - point_y, 2), 0.5)
+        power = 0.0 if distance > reach else pow(reach - distance, 2)
+        if max_power < power:
+            best_station, max_power = station, power
+    if not max_power:
+        return 'No link station within reach for point {},{}'.format(*device_point)
+    return 'Best link station for point {},{} is {},{} with power {:.1f}'.format(
+        *device_point, *best_station[:2], max_power
+    )
--- tests/test_lisle.py
+++ tests/test_lisle.py
@@ -9,7 +9,19 @@
 # Copyright (c) 2018, Pablo S. Blum de Aguiar <scorphus@gmail.com>
 
 from lisle import find_best_link_station
+from pytest import fixture, mark
 
 
-def test_find_best_link_station():
-    assert find_best_link_station(None, None)
+@fixture
+def link_stations():
+    return [(0, 0, 10), (20, 20, 5), (10, 0, 12)]
+
+
+@mark.parametrize('device_point, output_line', [
+    ((0, 0), 'Best link station for point 0,0 is 0,0 with power 100.0'),
+    ((100, 100), 'No link station within reach for point 100,100'),
+    ((15, 10), 'Best link station for point 15,10 is 10,0 with power 0.7'),
+    ((18, 18), 'Best link station for point 18,18 is 20,20 with power 4.7'),
+])
+def test_find_best_link_station(link_stations, device_point, output_line):
+    assert find_best_link_station(link_stations, device_point) == output_line
